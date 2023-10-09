756b81f2d7dfa845b848d8d797ffb731889f96d9
--replace-all <okiriotega@gmail.com>
2023-10-08 17:32:31 -0700

    task 5 print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry single digit
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int t;
+
+	for (t = 0 ; t < 10 ; t++)
+		printf("%d", t);
+	printf("\n");
+	return (0);
+}
