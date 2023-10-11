a59b2765d780b8e0a1c3ddf86e006e26de6da3ae
--replace-all <okiriotega@gmail.com>
2023-10-11 13:25:51 -0700

    task16 fibonacci98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,31 +6,36 @@
 
 int main(void)
 {
-	unsigned long int t, v, e, v1, v2, e1, e2;
+	unsigned long int t;
+	unsigned long int now = 1;
+	unsigned long int lat = 2;
+	unsigned long int T = 1000000000;
+	unsigned long int now1;
+	unsigned long int now2;
+	unsigned long int lat1;
+	unsigned long int lat2;
 
-	v = 1;
-	e = 2;
 
-	printf("%lu", v);
-	for (t = 1 ; t <= 91 ; t++);
+	printf("%lu", now);
+	for (t = 1 ; t < 91 ; t++);
 	{
-		printf(", %lu", e);
-		e = e + v;
-		v = e - v;
+		printf(", %lu", lat);
+		lat += now;
+		now = lat - now;
 	}
-	v1 = v / 1000000000;
-	v2 = v % 1000000000;
-	e1 = e / 1000000000;
-	e2 = e % 1000000000;
+	now1 = (now / T);
+	now2 = (now % T);
+	lat1 = (lat / T);
+	lat2 = (lat % T);
 
 	for (t = 92 ; t < 99 ; t++)
 	{
-		printf(", %lu", e1 + (e2 / 1000000000));
-		printf("%lu", e2 % 1000000000);
-		e1 = e1 + v1;
-		v1 = e1 - v1;
-		e2 = e2 + v2;
-		v2 = e2 - v2;
+		printf(", %lu", lat1 + (lat2 / T));
+		printf("%lu", lat2 % T);
+		lat1 = lat1 + now1;
+		now1 = lat1 - now1;
+		lat2 = lat2 + now2;
+		now2 = lat2 - now2;
 	}
 	printf("\n");
 	return (0);
