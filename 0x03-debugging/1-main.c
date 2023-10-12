15e2e080f1cfad071b3db717c6df32a748105344
--replace-all <okiriotega@gmail.com>
2023-10-12 07:50:48 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,11 +11,14 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	int i = 0;
-
+	
+	/*
 	while (i < 10)
 	{
 		putchar(i);
 	}
+	*/
+
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
