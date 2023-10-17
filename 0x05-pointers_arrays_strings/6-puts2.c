658abef809f513bb6e6f05622efabaa77d29726e
--replace-all <okiriotega@gmail.com>
2023-10-17 09:19:32 -0700

    ptr task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - print every other character of a string
+ * @str -
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int t;
+	for (t = 0 ; str[t] != '\0' ; t++)
+	{
+		if (t % 2 == 0)
+			_putchar(str[t]);
+	}
+	_putchar('\n');
+}
