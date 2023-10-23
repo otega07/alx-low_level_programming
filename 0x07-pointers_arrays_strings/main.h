a501ca85c68f1cb069c5246ad912fe86c3492993
--replace-all <okiriotega@gmail.com>
2023-10-23 16:36:55 -0700

    print_chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_i
+#define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -9,5 +9,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+int _putchar(char c);
 
 #endif /* MAIN_H */
