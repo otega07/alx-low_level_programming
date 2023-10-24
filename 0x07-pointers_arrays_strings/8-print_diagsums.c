a3162f449d9452a9272c52b37c7f56f4cdb092bf
--replace-all <okiriotega@gmail.com>
2023-10-23 17:04:49 -0700

    print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
  * print_diagsums - entry point
- * @a: input 1
- * @size: input 2
+ * @a: integer
+ * @size: size of matrix
  * Return: (0)
  */
 void print_diagsums(int *a, int size)
@@ -14,11 +14,15 @@ void print_diagsums(int *a, int size)
 
 	for (e = 0; e < size; e++)
 	{
-		sum1 = sum2 + a[e * size + e];
+		sum1 += a[e];
+		a += size;
 	}
+	a -= size;
+
 	for (e = size - 1; e >= 0; e--)
 	{
-		sum2 += a[e * size + (size - e - 1)];
+		sum2 += a[e];
+		a -= size;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
