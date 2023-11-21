902de76b70908a6da48234e228f8ab10e527c0fd
--replace-all <okiriotega@gmail.com>
2023-11-21 11:13:48 -0800

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,9 @@
 global main
-extern printf
+	extern printf
 main:
-	push rbp
 	mov rdi, format
 	xor eax, eax
 	call printf
-	pop rbp
 	mov eax, 0
 	ret
-format: db 'Hello, Holberton', 0
+format: db "Hello, Holberton", 0xA
