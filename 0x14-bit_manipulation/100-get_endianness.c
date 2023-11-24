704aabd36553d40ea6c7937a437f1d77f28bfc9d
--replace-all <okiriotega@gmail.com>
2023-11-23 20:54:02 -0800

    update for bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_endianness - a function that checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int t = 1;
+	int small_or_large;
+
+	small_or_large = (int) (((char *)&t)[0]);
+	return (small_or_large);
+}
